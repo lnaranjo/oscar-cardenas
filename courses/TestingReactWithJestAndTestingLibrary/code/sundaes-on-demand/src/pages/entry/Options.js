b5826d9a97fb965de452107aab007e0c91d7993d
ocardenasus <oscar.cardenas@unosquare.com>
2022-01-31 12:56:11 -0600

    feat: code and test scoop options from server

---
+++ courses/TestingReactWithJestAndTestingLibrary/code/sundaes-on-demand/src/pages/entry/Options.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+
+export default function Options({ optionType = 'scoops' }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3030/${optionType}`)
+      .then((response) => response.json())
+      .then(setItems)
+      .catch(console.error);
+  }, [optionType]);
+
+  // TODO: replace null with ToppingOption when available
+  const ItemComponent = (optionType === 'scoops' && ScoopOption) || null;
+
+  const optionItems = items.map(({ name, imagePath }) => (
+    <ItemComponent key={name} name={name} imagePath={imagePath} />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
