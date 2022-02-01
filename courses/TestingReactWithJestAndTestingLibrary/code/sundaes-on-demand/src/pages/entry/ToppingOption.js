88f271bfa6207c0346eeb264fd0b47c93c4b829a
ocardenasus <oscar.cardenas@unosquare.com>
2022-01-31 18:23:46 -0600

    feat: code and test topping total count

--- courses/TestingReactWithJestAndTestingLibrary/code/sundaes-on-demand/src/pages/entry/ToppingOption.js
+++ courses/TestingReactWithJestAndTestingLibrary/code/sundaes-on-demand/src/pages/entry/ToppingOption.js
@@ -1,13 +1,20 @@
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
-export default function ToppingOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath, updateItemCount }) {
+  function handleUpdate(event) {
+    updateItemCount(name, (event.target.checked && 1) || 0);
+  }
   return (
-    <Col xs={12} sm={4} lg={3} style={{ textAlign: 'center' }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: 'center' }}>
       <img
         alt={`${name} topping`}
         style={{ width: '75%' }}
         src={`http://localhost:3030/${imagePath}`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleUpdate} label={name} />
+      </Form.Group>
     </Col>
   );
 }
--- courses/TestingReactWithJestAndTestingLibrary/code/sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.js
+++ courses/TestingReactWithJestAndTestingLibrary/code/sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.js
@@ -30,3 +30,27 @@ test('update scoop subtotal when scoops changes', async () => {
   userEvent.type(chocolateInput, '2');
   expect(scoopSubtotal).toHaveTextContent('6.00');
 });
+
+test('update toppings subtotal when toppings change', async () => {
+  render(<Options optionType="toppings" />);
+
+  // make sure total starts out at $0.00
+  const toppingsTotal = screen.getByText('Toppings total: $', { exact: false });
+  expect(toppingsTotal).toHaveTextContent('0.00');
+
+  // add cherries and check subtotals
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  });
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
+
+  // add hot fudge and check subtotal
+  const hotFudgeCheckbox = screen.getByRole('checkbox', { name: 'Hot fudge' });
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent('3.00');
+
+  // remove hot fudge and check subtotal
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
+});
