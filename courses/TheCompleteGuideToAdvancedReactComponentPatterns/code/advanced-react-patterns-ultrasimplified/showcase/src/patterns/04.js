234dacf12198dd3814a72d09b84705e092e62306
ocardenasus <oscar.cardenas@unosquare.com>
2022-02-04 17:50:26 -0600

    feat: refactor to use reusables styles

--- courses/TheCompleteGuideToAdvancedReactComponentPatterns/code/advanced-react-patterns-ultrasimplified/showcase/src/patterns/04.js
+++ courses/TheCompleteGuideToAdvancedReactComponentPatterns/code/advanced-react-patterns-ultrasimplified/showcase/src/patterns/04.js
@@ -12,6 +12,7 @@ import mojs from 'mo-js';
 import { generateRandomNumber } from '../utils/generateRandomNumber';
 
 import styles from './index.css';
+import userCustomStyles from './usage.css';
 
 /** ====================================
  *      Define custom context
@@ -120,15 +121,25 @@ const useClapAnimation = ({ clapEl, clapCountEl, clapTotalEl }) => {
  *      🔰SubComponents
 Smaller Component used by <MediumClap />
 ==================================== **/
-const ClapIcon = () => {
+const ClapIcon = ({ style: userStyle = {}, className }) => {
   const { isClicked } = useContext(MediumClapContext);
+
+  const classNames = [
+    styles.icon,
+    className,
+    (isClicked && styles.checked) || '',
+  ]
+    .join(' ')
+    .trim();
+
   return (
     <span>
       <svg
         id="clapIcon"
-        xmlns="http://www.w3.org/2000/svg"
+        style={userStyle}
+        className={classNames}
         viewBox="-549 338 100.1 125"
-        className={`${styles.icon} ${isClicked && styles.checked}`}
+        xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z" />
         <path d="M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9" />
@@ -137,19 +148,31 @@ const ClapIcon = () => {
   );
 };
 
-const ClapCount = () => {
+const ClapCount = ({ style: userStyle = {}, className }) => {
   const { count, setRef } = useContext(MediumClapContext);
+  const classNames = [styles.count, className].join(' ').trim();
   return (
-    <span ref={setRef} data-refkey="clapCountRef" className={styles.count}>
+    <span
+      ref={setRef}
+      style={userStyle}
+      className={classNames}
+      data-refkey="clapCountRef"
+    >
       +{count}
     </span>
   );
 };
 
-const CountTotal = () => {
+const CountTotal = ({ style: userStyle = {}, className }) => {
   const { countTotal, setRef } = useContext(MediumClapContext);
+  const classNames = [styles.totals, className].join(' ').trim();
   return (
-    <span ref={setRef} data-refkey="clapTotalRef" className={styles.total}>
+    <span
+      ref={setRef}
+      style={userStyle}
+      className={classNames}
+      data-refkey="clapTotalRef"
+    >
       {countTotal}
     </span>
   );
@@ -166,7 +189,12 @@ const initialState = {
   countTotal: generateRandomNumber(500, 10000),
 };
 
-const MediumClap = ({ children, onClap }) => {
+const MediumClap = ({
+  onClap,
+  children,
+  className,
+  style: userStyles = {},
+}) => {
   const [{ clapRef, clapCountRef, clapTotalRef }, setRefs] = useState({});
   const [clapState, setClapState] = useState(initialState);
   const { count } = clapState;
@@ -197,7 +225,6 @@ const MediumClap = ({ children, onClap }) => {
   }, []);
 
   const handleClapClick = () => {
-    // 👉 prop from HOC
     animationTimeline.replay();
 
     setClapState((prevState) => ({
@@ -215,12 +242,14 @@ const MediumClap = ({ children, onClap }) => {
     [clapState, setRef]
   );
 
+  const classNames = [styles.clap, className].join(' ').trim();
   return (
     <Provider value={memoizedValues}>
       <button
         ref={setRef}
+        style={userStyles}
         data-refkey="clapRef"
-        className={styles.clap}
+        className={classNames}
         onClick={handleClapClick}
       >
         {children}
@@ -244,10 +273,10 @@ const Usage = () => {
   const handleClap = (clapState) => setCount(clapState.count);
   return (
     <div style={{ width: '100%' }}>
-      <MediumClap onClap={handleClap}>
-        <MediumClap.Icon />
-        <MediumClap.Count />
-        <MediumClap.Total />
+      <MediumClap onClap={handleClap} className={userCustomStyles.clap}>
+        <MediumClap.Icon className={userCustomStyles.icon} />
+        <MediumClap.Count className={userCustomStyles.count} />
+        <MediumClap.Total className={userCustomStyles.total} />
       </MediumClap>
       {!!count && <div className={styles.info}>You have clap: {count}</div>}
     </div>
